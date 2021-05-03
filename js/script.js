9a5c4a3de8b65d88e79850650f515f00b6bd99f6
sasjakoning <sasjakoning@hotmail.com>
2021-05-03 20:37:26 +0200

    added homescreen redirect delay and clearer comments

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
+
+// stukje code voor homescreen animaties
+
 const startButton = document.querySelector(".homepagebuttoncontainer");
 
 const homepagesVisuals = document.querySelectorAll(".visualcontainer > div");
 
-console.log(homepagesVisuals);
+// voeg aan elke div in de visualcontainer een class toe met een animation erin
 
 startButton.addEventListener("click", function () {
   homepagesVisuals.forEach(function (element) {
     element.classList.add("homepageAnim");
   });
   startButton.classList.add("homepagebuttonAnim");
+  
+  redirectHomescreen()
 });
 
-// todo: homepage redirect link delay for anims
+// delay op de redirect zodat de animaties eerst afspelen
+
+function redirectHomescreen() {
+    console.log("aaa")
+  var tID = setTimeout(function () {
+    window.location.href =
+      "todolists.html";
+    window.clearTimeout(tID); // clear time out.
+  }, 2500);
+}
+//  bron redirect: https://www.encodedna.com/javascript/redirect-page-after-a-delay-using-javascript.htm
